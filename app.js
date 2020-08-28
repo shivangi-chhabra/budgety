05e91fc82130cc037cc320d6a251074abf447a7a
shivangi-chhabra <40178102+shivangi-chhabra@users.noreply.github.com>
2020-08-29 02:42:30 +0530

    updating percentage on UI

--- app.js
+++ app.js
@@ -171,7 +171,8 @@ var UIController = (function() {
        incomeLabel: '.budget__income--value',
        expensesLabel: '.budget__expenses--value',
        percentageLabel: '.budget__expenses--percentage',
-       container: '.container'
+       container: '.container',
+       expensesPercentageLabel: '.item__percentage'
     };
     return {
       getinput: function(){
@@ -241,6 +242,30 @@ var UIController = (function() {
 
       },
 
+      displayPercentages: function(percentages) {
+         
+        var fields = document.querySelectorAll(DOMstrings.expensesPercentageLabel);
+        
+        var nodeListForEach = function(list, callback) {
+            for (var i = 0; i < list.length; i++) {
+              callback(list[i], i);
+            }
+
+        };
+
+        nodeListForEach(fields, function(current, index) {
+          
+          if (percentages[index] > 0) {
+            current.textContent = percentages[index] + '%';
+          } else {
+            current.textContent = percentages[index] + '---';
+          }
+          
+
+        });
+
+      },
+
       getDOMstrings: function(){
         return DOMstrings;
       }
@@ -289,7 +314,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         var percentages = budgetCtrl.getPercentages();
 
         // 3. Update the UI with the new percentages
-        console.log(percentages);
+        UICtrl.displayPercentages(percentages);
     };
 
     var ctrlAddItem = function() {
